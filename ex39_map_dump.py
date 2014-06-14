b25ebf59640b9d41379af5008b1c70b750bee13d
Rogier Schaap <reschaap@gmail.com>
2014-06-14 19:56:40 +0200

    Initital commit
    Exercises for Learning Python the Hard Way
    
     Initial commit
    	new file:   ex38.py
    	new file:   ex39.py
    	new file:   ex39_map.py
    	new file:   ex39_map_assert.py
    	new file:   ex39_map_dump.py

---
+++ ex39_map_dump.py
@@ -0,0 +1,138 @@
+def Map(num_buckets=256):
+    """Initializes a Map with the given number of buckets."""
+    aMap = []
+    for i in range(0, num_buckets):
+        aMap.append([])
+    return aMap
+
+def Map_hash(aMap, key):
+    """Given a key this will create a number and then convert it to
+    and index for the aMap's buckets."""
+    return hash(key) % len(aMap)
+
+def Map_get_bucket(aMap, key):
+    """Given a key, find the bucket where it would go"""
+    bucket_id = Map_hash(aMap, key)
+    return aMap[bucket_id]
+
+def Map_get_slot(aMap, key, default=None):
+    """Returns the index, key, and value of a slot found in a bucket."""
+    bucket = Map_get_bucket(aMap, key)
+    
+    for i, kv in enumerate(bucket):
+        k, v = kv
+        if key == k:
+            return i, k, v
+        
+    return -1, key, default
+
+def Map_get(aMap, key, default=None):
+     """Gets the value in a bucket for the given key, or the default."""
+     i, k, v = Map_get_slot(aMap, key, default=default)
+     return v
+ 
+def Map_set(aMap, key, value):
+     """Sets the key to the value, replacing any existing value."""
+     bucket = Map_get_bucket(aMap, key)
+     i, k, v = Map_get_slot(aMap, key)
+     
+     if v:
+         bucket[i] = (key, value)
+     else:
+         bucket.append((key, value))
+
+def Map_delete(aMap, key):
+    """Deletes the given key from the Map"""
+    bucket = Map_get_bucket(aMap, key)
+    
+    for i in xrange(len(bucket)):
+        k, v = bucket[i]
+        if key == k:
+            del bucket[i]
+            break
+
+def Map_list(aMap):
+    """Prints out what's in the Map"""
+    full_buckets = 0
+    for bucket in aMap:
+        if bucket:
+            full_buckets += 1
+            for k, v in bucket:
+                print k, v
+    return full_buckets
+
+def Map_dump(aMap):
+    """Dumps the contents of every bucket"""
+    # First get the buckets
+    for i, b in enumerate(aMap):
+        # Get the contents of the bucket only if it not empty
+        if b != []:
+            print "Bucket nr: %r" % i, " has this content: %r" % b
+
+def Map_clean(aMap):
+    """Removes all items from aMap"""
+    # First get the full buckets
+    for i, bucket in enumerate(aMap):
+        if bucket != []:
+            aMap[i] = []
+
+def Map_has_key(aMap, key):
+    """Checks whether a key exists in aMap."""
+    i, k, v = Map_get_slot(aMap, key, None)
+    
+    if v:
+        return True
+    
+    return False
+
+def Map_items(aMap):
+    """Returns a list of key, value pairs (tupple)"""
+    
+# The tests that it will work.
+
+jazz = Map()
+
+Map_set(jazz, 'Miles Davis', 'Flamenco Sketches')
+# confirms set will replace previous one
+Map_set(jazz, 'Miles Davis', 'Kind Of Blue')
+Map_set(jazz, 'Duke Ellington', 'Beginning To See The Light')
+Map_set(jazz, 'Billy Strayhorn', 'Lush Life')
+
+print "---- Dump test ----"
+Map_dump(jazz)
+
+print "---- Clean test ----"
+Map_clean(jazz)
+
+assert Map_list(jazz) == 0
+
+Map_set(jazz, 'Miles Davis', 'Kind Of Blue')
+Map_set(jazz, 'Duke Ellington', 'Beginning To See The Light')
+Map_set(jazz, 'Billy Strayhorn', 'Lush Life')
+print "---- List Test ----"
+Map_list(jazz)
+
+print "---- Get Test ----"
+assert Map_get(jazz, 'Miles Davis') == 'Kind Of Blue'
+assert Map_get(jazz, 'Duke Ellington') == 'Beginning To See The Light'
+assert Map_get(jazz, 'Billy Strayhorn') == 'Lush Life'
+
+print "---- Has Key test ----"
+assert Map_has_key(jazz, 'Miles Davis')
+
+print "---- Delete Test ----"
+print "** Goodbye Miles **"
+Map_delete(jazz, "Miles Davis")
+assert Map_get(jazz, 'Miles Davis') == None
+
+print "** Goodbye Duke"
+Map_delete(jazz, "Duke Ellington")
+assert Map_get(jazz, 'Duke Ellington') == None
+
+print "** Goodbye Billy **"
+Map_delete(jazz, "Billy Strayhorn")
+assert Map_get(jazz, 'Billy Strayhorn') == None
+
+print "** Goodbye Pork Pie Hat **"
+Map_delete(jazz, "Charles Mingus")
+assert Map_get(jazz, 'Charles Mingus') == None

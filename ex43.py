0fcd2b40c2032bab1df92ab794eb715c79840de7
Rogier Schaap <r.e.j.schaap@umcg.nl>
2014-07-22 17:20:28 +0200

    Modified the code in ex43.py.
    Attributes are now used instead of return's.
    
    Signed-off-by: Rogier Schaap <r.e.j.schaap@umcg.nl>

--- ex43.py
+++ ex43.py
@@ -13,22 +13,23 @@ class Scene (object):
     def choice(self):
         print self.choice_description
         choice = raw_input('>..')
-        print self.key
-        print choice
         self.scene_result = (self.key, choice)
 
 
 class Engine(object):
     
-    def __init__(self, scene_map):
-       self.scene_map = scene_map
+    def __init__(self, map):
+       self.map = map
     
     def play(self):
-        scene = self.scene_map.next_scene(None)
-        scene_result = scene.enter()
+        self.map.next_scene(None)
+        scene = self.map.new_scene
+        scene.enter()
+        scene_result = scene.scene_result
         
         while scene_result != None:
-           scene = self.scene_map.next_scene(scene_result)
+           self.map.next_scene(scene_result)
+           scene = self.map.new_scene
            scene_result = scene.enter()
            
         print "Thank you for playing."
@@ -52,8 +53,7 @@ class LaserWeaponArmory(Scene):
     
     enter_description = "LaserWeaponArmory enter description."
     choice_description = "Press 1"
-    def enter(self):
-        pass
+    key = 'laser_weapon_armory'
 
 
 class TheBridge(Scene):
@@ -70,19 +70,20 @@ class EscapePod(Scene):
 
 class Map(object):
     
-    def __init__(self, start_scene):
-        self.start_scene = start_scene
-        self.scene_structure = { 'death' :  [Death(), CentralCorridor(), None], 
+    new_scene = None
+    scene_structure = { 'death' :  [Death(), CentralCorridor(), None], 
         'central_corridor' :  [CentralCorridor(), Death(), LaserWeaponArmory()]
         , 'laser_weapon_armory' : [LaserWeaponArmory(), None] }
-    
+        
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
     
     def next_scene(self, scene_result):
         if self.start_scene == None:
             key, choice = scene_result
             scene_list = self.scene_structure[key]
-            scene = scene_list[choice]
-            return scene
+            self.new_scene = scene_list[int(choice)]
         
         else:
             self.opening_scene()
@@ -90,15 +91,11 @@ class Map(object):
     
     def opening_scene(self):
         scene_list = self.scene_structure[self.start_scene]
-        scene = scene_list[0]
+        self.new_scene = scene_list[0]
         self.start_scene = None
-        return scene
 
 
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
-#a_game.play()
+a_game.play()
 
-a_scene = Death()
-a_scene.enter()
-print a_scene.scene_result

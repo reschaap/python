1308da89ff43e3e3e2887eb40a4fe725e623a2bc
Rogier Schaap <reschaap@gmail.com>
2014-11-12 23:20:48 +0100

    Started work on excercise 45.
    	new file:   ex45 notes
    	new file:   ex45.py
    	new file:   ex45_test.py
    	new file:   test_texts.py

---
+++ ex45_test.py
@@ -0,0 +1,9 @@
+import textwrap
+import test_texts
+
+print test_texts.central_corridor1
+text = test_texts.central_corridor2
+print text
+
+print textwrap.wrap(test_texts.central_corridor1)
+#print textwrap.fill(test_texts.central_corridor2)

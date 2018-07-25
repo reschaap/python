4f342e2b74cb1f8cb7875123059393f5710f7be5
Rogier Schaap <r.e.j.schaap@umcg.nl>
2018-07-25 14:11:48 +0200

    First working version of Intro 21.

---
+++ CodeFight/Intro 21.py
@@ -0,0 +1,36 @@
+from util import br
+
+def isIPv4Address(inputString):
+    input_list = inputString.split('.')
+#    import pdb; pdb.set_trace()
+    if len(input_list) != 4:
+        return False
+    else:
+        try:
+            for num in input_list:
+                if int(num) < 0 or int(num) > 255:
+                    return False
+                else:
+                    continue
+        except:
+            return False
+    return True
+
+
+# Client
+s1 = "172.16.254.1"
+s2 = "172.316.254.1"
+s3 = "1.1.1.1a"
+s4 = "7283728"
+s5 = "1.23.256.."
+
+print(isIPv4Address(s1))
+br()
+print(isIPv4Address(s2))
+br()
+print(isIPv4Address(s3))
+br()
+print(isIPv4Address(s4))
+br()
+print(isIPv4Address(s5))
+br()
